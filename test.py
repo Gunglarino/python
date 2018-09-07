aeedefde42d2e15e50fb2cf71875d898afb4acd5
Thomas van den Berg <Thomasvandenberg98@hotmail.com>
2018-09-07 13:43:12 +0200

    Assignments first week

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-string = "Mijn naam is: "
-naam = "Thomas van den Berg"
-print(string,naam)
+a = 6
+b = 7
+c = (a+b)/2
+#print(c)
+voornaam = "Thomas"
+tussenvoegsel = "van den"
+achternaam = "Berg"
+mijnNaam = voornaam+ " " + tussenvoegsel + " " + achternaam
+#print(mijnNaam)
+print(75>a and 75<b)
+print(len(mijnNaam) == len(voornaam)+len(tussenvoegsel)+len(achternaam))
+print(len(mijnNaam) > len(tussenvoegsel)*5)
+print(tussenvoegsel in achternaam)
