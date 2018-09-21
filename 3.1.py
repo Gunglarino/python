f7066046f45e388c5df31b62ff9808a08fa49ef3
Thomas van den Berg <Thomasvandenberg98@hotmail.com>
2018-09-21 12:26:00 +0200

    3.1

---
+++ 3.1.py
@@ -0,0 +1,10 @@
+icor = 6
+prog = 8
+csn = 10
+
+tot = sum([icor, prog, csn])
+avg = tot / 3
+rew = tot * 30
+
+print("Je gemiddelde is een: "+str(avg))
+print("Als beloning voor je cijfers krijg je: €"+ str(rew))
