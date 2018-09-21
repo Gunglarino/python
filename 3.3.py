6cf158a10d9b5b516e7148b985cb0c67ad2f0bcd
Thomas van den Berg <Thomasvandenberg98@hotmail.com>
2018-09-21 12:58:46 +0200

    3.3

---
+++ 3.3.py
@@ -0,0 +1,5 @@
+uurloon = float(input("Wat krijg je per uur betaalt?"))
+gewerkt = int(input("Hoeveel uur heb je gewerkt?"))
+
+salaris = uurloon * gewerkt
+print(str(gewerkt) +" uur werken levert " + str(salaris) +" Euro op")
