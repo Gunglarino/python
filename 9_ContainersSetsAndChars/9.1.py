8d8ce9ac13bb94379022382295a173d93b81a63c
Thomas "Gungla" van den Berg <thomasvandenberg98@hotmail.com>
2018-10-12 14:44:57 +0200

    9.1

---
+++ 9_ContainersSetsAndChars/9.1.py
@@ -0,0 +1,5 @@
+bruin = {"Boxtel", "Best", "Buikenlaan", "Eindhoven", "Helmond 't Hout", "Helmond", "Helmond Brouwhuis", "Deurne"}
+groen = {"Boxtel", "Best", "Buikenlaan", "Eindhoven", "Geldrop", "Heeze", "Weert"}
+print(bruin.intersection(groen))
+print(bruin.difference(groen))
+print(bruin.union(groen))
